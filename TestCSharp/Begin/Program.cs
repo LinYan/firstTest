cba9a41917226d1f17ce916779dbdcd295372b14
YanLin <415668119@qq.com>
2015-02-17 14:33:31 +0800

    initial project version

---
+++ TestCSharp/Begin/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestCSharp;
+namespace Begin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OK a = new OK();
+            Console.WriteLine("sdasda");
+            Console.Read();
+
+        }
+    }
+}
